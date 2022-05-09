caeebd5bb7fb9ea0dad6db4f52a37464a3b49605
Andrew <s.andrea.storci@gmail.com>
2022-05-09 22:30:27 +0200

    Added planimetry on pages

--- php/deleteProduct.php
+++ php/deleteProduct.php
@@ -4,7 +4,7 @@ require_once('connessione.php');
 $idProd = isset($_POST['idProd']) ? $_POST['idProd'] : 0;
 if(!$idProd == 0){
   $delete = "DELETE FROM prodotti WHERE id = " . $idProd;
-  $result = $conn->query($delete);
+  $result = $pdo->query($delete);
   if($result)
     echo 1;
   else
