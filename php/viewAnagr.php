e353830713e3f92f16523ccac9d51c93648acc2b
CristianF03 <cfiliberti@studenti.iissgadda.it>
2022-05-10 18:43:07 +0200

    aggiunta visualizza anagrafica

---
+++ php/viewAnagr.php
@@ -0,0 +1,17 @@
+<?php
+require_once('connessione.php');
+
+$query="SELECT nome,sede,path_logo FROM anagrafica";
+$risultato=$pdo->query($query);
+$array = array( );
+$i=0;
+while($row=$risultato->fetch(PDO::FETCH_ASSOC)){
+  $array[$i]=array(
+    "nome"=>$row["nome"],
+    "sede"=>$row["sede"],
+    "path_logo"=>$row["path_logo"]
+  );
+  $i++;
+}
+echo json_encode($array);
+?>
