1b6ee266c5929fbdd55ce405f8f72d5abb3ebd39
Andrew <75398539+AndrewStorci7@users.noreply.github.com>
2022-07-04 17:12:55 +0200

    Update deleteProduct.php

--- php/deleteProduct.php
+++ php/deleteProduct.php
@@ -5,7 +5,7 @@ $idProd = isset($_POST['id']) ? $_POST['id'] : 0;
 if(!$idProd == 0){
   $delete = "DELETE FROM prodotti WHERE id = :id";
   $res = $pdo->prepare($delete);
-  $res->bindParam(":id", $idProd, PDO::PARAM_STR);
+  $res->bindParam(":id", $idProd, PDO::PARAM_INT);
   $res->execute();
   if($res)
     echo 1;
