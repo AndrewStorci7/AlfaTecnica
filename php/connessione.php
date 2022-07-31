27fe36ddf907b91909e3635c074360a47c1a0976
TheCourierNV <andreascart04@gmail.com>
2022-08-01 01:49:24 +0200

    Changed db user away from root
    This is nowhere near being a proper fix, but at least it avoids someone
    setting up the root user for remote login I guess

--- php/connessione.php
+++ php/connessione.php
@@ -3,7 +3,7 @@ $db = [
     'db_engine' => 'mysql',
     'db_host' => '127.0.0.1',
     'db_name' => 'alfatecnica',
-    'db_user' => 'root',
+    'db_user' => 'alfatecnica',
     'db_password' => '',
 ];
 
